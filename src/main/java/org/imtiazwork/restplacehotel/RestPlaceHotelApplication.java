fb7c14a1108eeb59f23bbb69925cdab31e58de66
imtiaz-workplace-admin <imtiazmahmud6272@gmail.com>
2025-01-01 12:40:29 +0600

    Initial commit

---
+++ src/main/java/org/imtiazwork/restplacehotel/RestPlaceHotelApplication.java
@@ -0,0 +1,13 @@
+package org.imtiazwork.restplacehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPlaceHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestPlaceHotelApplication.class, args);
+    }
+
+}
